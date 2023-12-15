c96293b7fb036558c89e7ad66a10b99969aaa564
unknown <slavevskidavid@gmail.com>
2023-12-15 09:36:39 +0100

    aritmeticka sredina

---
+++ src/aritmetickasredina/AritmetickaSredina.java
@@ -0,0 +1,21 @@
+package aritmetickasredina;
+
+public class AritmetickaSredina {
+	int a;
+	int b;
+	int c;
+AritmetickaSredina(int a, int b, int c){
+	this.a = a;
+	this.b = b;
+	this.c = c;
+}
+void calculateAS() {
+	System.out.println((a + b + c) / 3);
+}
+public static void main(String args[]) {
+	AritmetickaSredina firstN = new AritmetickaSredina ( 3 , 5 , 7 );
+	AritmetickaSredina secondN = new AritmetickaSredina ( 13 , 25 , 29);
+	firstN.calculateAS();
+	secondN.calculateAS();
+}
+}
